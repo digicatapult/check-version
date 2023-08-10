1030c8e5796505293751bd6582404cdf671c8662
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-08-10 13:25:23 +0100

    added inputs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
 import {checkVersion} from './check-version'
+const github = require('@actions/github')
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     const location: string = core.getInput('location')
+    const owner: string = core.getInput('owner')
+    const repo: string = core.getInput('repo')
+    const token: string = core.getInput('token')
+
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
@@ -15,6 +20,10 @@ async function run(): Promise<void> {
     core.setOutput('time', new Date().toTimeString())
 
     core.debug('location')
+
+    //instance of octokit to call GitHub's Rest API
+    const octokit = new github.getOctokit(token)
+
     await checkVersion(location)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
