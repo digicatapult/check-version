7179b241bf678aa9c573846470447cccf3a6ae78
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-08-16 11:51:49 +0100

    testing again

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,6 @@ const location: string = core.getInput('location')
 
 async function run(core: TypeOfCore): Promise<void> {
   try {
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-
     // await checkVersion(location, ghToken)
     const checkVersion = new CheckVersion(core, fs)
     await checkVersion.checkVersion(location, ghToken)
