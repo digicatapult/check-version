3e855c6f74d105de1590cf452eeab712c58a06c2
Ellenn-A <58051865+Ellenn-A@users.noreply.github.com>
2023-08-17 12:44:30 +0100

    Check version with release and test.yaml Feature/ver 77 (#14)
    implemented V1 of check-version action

---
+++ src/__tests__/checkVersion.test.ts
@@ -0,0 +1,89 @@
+import {describe, test} from 'mocha'
+import {expect} from 'chai'
+import sinon from 'sinon'
+import fs from 'fs/promises'
+import * as core from '@actions/core'
+import {CheckVersion} from '../checkVersion'
+import {GetFiles} from '../getFiles'
+import {dummyData, expectedArray, Tag, TestData} from './testData'
+
+describe('checkVersion', function () {
+  afterEach(() => {
+    // Restore the default sandbox here
+    sinon.restore()
+  })
+
+  test('reads files in location errors if it does not find two files with package in name', async function () {
+    const readdirStub = sinon.stub(fs, 'readdir').resolves([])
+    let error: Error | unknown = null
+    try {
+      const getFiles = new GetFiles(fs)
+      await getFiles.getFiles('some/location')
+    } catch (err: any) {
+      if (err instanceof Error) {
+        error = err
+      }
+    }
+
+    expect(error).instanceOf(Error)
+  })
+
+  test('compare versions failed stubx not called - same', async function () {
+    const setFailedStubx = sinon.stub(core, 'setFailed')
+    const readdirStub = sinon.stub(fs, 'readdir').resolves([])
+    const checkVersion = new CheckVersion(core, fs)
+
+    await checkVersion.compareVersions('1.1.1', '1.1.1')
+
+    expect(setFailedStubx.calledOnce).to.equal(false)
+  })
+
+  test('compare versions failed stubx not called - not the same ', async function () {
+    const setFailedStubx = sinon.stub(core, 'setFailed')
+
+    const checkVersion = new CheckVersion(core, fs)
+
+    await checkVersion.compareVersions('1.1.1', '2.1.1')
+
+    expect(setFailedStubx.calledOnce).to.equal(true)
+  })
+
+  test('filter through an array of tags and return sorted ones per semver rules', async function () {
+    const checkVersion = new CheckVersion(core, fs)
+    const testData = new TestData()
+
+    const res: Tag[] = await checkVersion.filterTags(dummyData)
+
+    expect(res[res.length - 1].name).to.equal('1.2.0')
+    expect(res.length).to.equal(expectedArray.length)
+  })
+
+  test('assert comparissons - pass  ', async function () {
+    const setFailedStubx = sinon.stub(core, 'setFailed')
+    const checkVersion = new CheckVersion(core, fs)
+
+    let res = await checkVersion.assertComparisons('1.1.1', '2.1.1')
+
+    expect(setFailedStubx.calledOnce).to.equal(false)
+    expect(res).to.equal(true)
+  })
+
+  test('assert comparissons - fail  ', async function () {
+    const setFailedStubx = sinon.stub(core, 'setFailed')
+    const checkVersion = new CheckVersion(core, fs)
+
+    let res = await checkVersion.assertComparisons('1.1.1', '0.1.1')
+
+    expect(setFailedStubx.calledOnce).to.equal(true)
+    expect(res).to.equal(false)
+  })
+
+  test('assert comparissons - pass  ', async function () {
+    const setFailedStubx = sinon.stub(core, 'setFailed')
+    const checkVersion = new CheckVersion(core, fs)
+
+    let res = await checkVersion.assertComparisons('0.1.1', '0.1.1')
+
+    expect(res).to.equal(true)
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs/promises'
+import {CheckVersion} from './checkVersion'
 
-async function run(): Promise<void> {
+type TypeOfCore = typeof core
+
+const ghToken: string = core.getInput('token')
+const ms: string = core.getInput('milliseconds')
+const location: string = core.getInput('npm_package_location')
+
+async function run(core: TypeOfCore): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // await checkVersion(location, ghToken)
+    const checkVersion = new CheckVersion(core, fs)
+    await checkVersion.checkVersion(location, ghToken)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run()
+run(core)
