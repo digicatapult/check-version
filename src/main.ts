245d1e241ea725adf6cd424e1cd1e092d149621b
Jonathan Gray <jonmattgray@gmail.com>
2023-08-23 13:53:48 +0100

    Add `failOnSameVersion` input (#28)
    * fail on same version
    
    * update renovate and packages
    
    * open sourcing docs
    
    * improve input casting

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import {CheckVersion} from './checkVersion'
+import {stringToBoolean} from './util'
 
 type TypeOfCore = typeof core
 
 const ghToken: string = core.getInput('token')
-const ms: string = core.getInput('milliseconds')
 const location: string = core.getInput('npm_package_location')
+const failOnSameVersion = core.getInput('fail_on_same_version')
 
 async function run(core: TypeOfCore): Promise<void> {
   try {
-    // await checkVersion(location, ghToken)
     const checkVersion = new CheckVersion(core, fs)
-    await checkVersion.checkVersion(location, ghToken)
+    await checkVersion.checkVersion(
+      location,
+      ghToken,
+      stringToBoolean(failOnSameVersion)
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
