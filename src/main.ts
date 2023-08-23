35c2a751e415212f1e5a4fd024055fbc3b039d43
Jonathan Gray <jonmattgray@gmail.com>
2023-08-23 12:24:38 +0100

    improve input casting

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import {CheckVersion} from './checkVersion'
+import {stringToBoolean} from './util'
 
 type TypeOfCore = typeof core
 
 const ghToken: string = core.getInput('token')
 const location: string = core.getInput('npm_package_location')
-const failOnSameVersion = Boolean(core.getInput('fail_on_same_version'))
+const failOnSameVersion = core.getInput('fail_on_same_version')
 
 async function run(core: TypeOfCore): Promise<void> {
   try {
     const checkVersion = new CheckVersion(core, fs)
-    await checkVersion.checkVersion(location, ghToken, failOnSameVersion)
+    await checkVersion.checkVersion(
+      location,
+      ghToken,
+      stringToBoolean(failOnSameVersion)
+    )
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
