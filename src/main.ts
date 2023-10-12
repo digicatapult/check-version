84b56088d035904857674bbeaeb846b38d1dc071
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-10-12 13:55:50 +0100

    passing in arguments

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,34 @@
 import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import {CheckVersion} from './lib/checkVersion'
-import {stringToBoolean} from './util'
+import {assignManager, stringToBoolean} from './util'
 
 type TypeOfCore = typeof core
 
 const ghToken: string = core.getInput('token')
-const manager: string = core.getInput('package_manager')
-const location: string = core.getInput('location')
+const npmLocation: string = core.getInput('npm_package_location')
+const cargoLocation: string = core.getInput('cargo_package_location')
+const poetryLocation: string = core.getInput('poetry_package_location')
 const failOnSameVersion: string = core.getInput('fail_on_same_version')
 
 async function run(core: TypeOfCore): Promise<void> {
   try {
+    const packageManager = await assignManager(
+      npmLocation,
+      cargoLocation,
+      poetryLocation
+    )
+    if (!packageManager) {
+      throw new Error(
+        `There has been an issue while assigning a package manager and location.`
+      )
+    }
     const checkVersion = new CheckVersion(core, fs)
     await checkVersion.checkVersion({
-      location: location,
+      location: packageManager.location,
       ghToken,
       failOnSameVersion: stringToBoolean(failOnSameVersion),
-      manager
+      manager: packageManager.manager
     })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
--- src/util.ts
+++ src/util.ts
@@ -8,3 +8,19 @@ export const stringToBoolean = (input: string): boolean => {
   }
   throw new Error(`${input} is invalid. Must be 'true' or 'false'`)
 }
+
+export async function assignManager(
+  npmLocation: string,
+  cargoLocation: string,
+  poetryLocation: string
+) {
+  if (npmLocation.length > 1) {
+    return {location: npmLocation, manager: 'npm'}
+  }
+  if (cargoLocation.length > 1) {
+    return {location: cargoLocation, manager: 'cargo'}
+  }
+  if (poetryLocation.length > 1) {
+    return {location: poetryLocation, manager: 'poetry'}
+  }
+}
