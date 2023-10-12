64d0ceb7377cbc0ca285ea3e4f40ee9f49bbe95c
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2023-10-12 12:33:11 +0100

    removed duplicate check

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import {CheckVersion} from './lib/checkVersion'
 import {stringToBoolean} from './util'
 
 type TypeOfCore = typeof core
-const managerTypes = ['npm', 'cargo', 'poetry']
 
 const ghToken: string = core.getInput('token')
 const manager: string = core.getInput('package_manager')
@@ -13,10 +12,6 @@ const failOnSameVersion: string = core.getInput('fail_on_same_version')
 
 async function run(core: TypeOfCore): Promise<void> {
   try {
-    if (!managerTypes.includes(manager)) {
-      core.setFailed(`Unknown type of manager`)
-      throw new Error(`Unknown type of manager`)
-    }
     const checkVersion = new CheckVersion(core, fs)
     await checkVersion.checkVersion({
       location: location,
